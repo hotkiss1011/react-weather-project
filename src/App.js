1c79bc0e2aef94f523f0929d2aaab98da5e49275
Amanda Hotchkiss <amandapandah@yahoo.com>
2021-09-13 20:56:56 -0400

    Added GitHub
    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This was coded by Amanda Hotchkiss and is open-sourced on {" "} 
+        <a href="https://github.com/hotkiss1011/react-weather-project" target="_blank">GitHub</a>
+      </footer>
     </div>
   );
 }
