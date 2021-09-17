9f85aae479f374278ff554c31dc50f0a543f6c38
Amanda Hotchkiss <amandapandah@yahoo.com>
2021-09-17 19:43:44 -0400

    added Netlify link

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This was coded by {" "}
-          <a href="https://adoring-haibt-562efa.netlify.app/">Amanda Hotchkiss</a>
-          {" "} and is open-sourced on {" "} 
-          <a href="https://github.com/hotkiss1011/react-weather-project">GitHub</a>
+          This was coded by{" "}
+          <a href="https://adoring-haibt-562efa.netlify.app/">
+            Amanda Hotchkiss
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a href="https://github.com/hotkiss1011/react-weather-project">
+            GitHub
+          </a>{" "}
+          and hosted using <a href="https://www.netlify.com/">Netlify</a>.
         </footer>
       </div>
     </div>
