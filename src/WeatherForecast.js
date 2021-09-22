00899f942c4515dfc381b2bf1eb82a4ea54c056a
Amanda Hotchkiss <amandapandah@yahoo.com>
2021-09-22 19:32:58 -0400

    added refresh to forecast
    forecast now updates when city is updated

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
-import "./WeatherForecast.css"
+import React, { useEffect, useState } from "react";
+import "./WeatherForecast.css";
 import axios from "axios";
-import WeatherForecastDay from "./WeatherForecastDay"
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coord])
 
   function handleResponse(response) {
     setForecast(response.data.daily);
